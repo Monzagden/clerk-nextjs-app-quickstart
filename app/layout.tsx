3135be1c4386b0bcac016f6d5bdb1cb820fab249
Moncef Zaghdane <moncef_zaghdane@hotmail.com>
2024-05-12 20:31:51 +0200

    testatat

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return ( 
   
     <html>
-      <></>
+      <>
       
       <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
           <nav className="h-14 flex items-center justify-between px-4">
